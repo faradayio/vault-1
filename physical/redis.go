d1dfb00f089d39399fec36a7997f4726b339b786
Eric Kidd <git@randomhacks.net>
2016-07-12 15:09:45 -0400

    redis: Implement Put, Get, Delete and List
    This provides an ordinary vault storage backend.   We don't actually
    need one, because we use S3 for actual storage, but we need to implement
    this before we implement the optional HA backend for Redis (which is
    what we really want).

--- physical/redis.go
+++ physical/redis.go
@@ -1,6 +1,7 @@
 package physical
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
@@ -46,19 +47,85 @@ func newRedisBackend(conf map[string]string, logger *log.Logger) (Backend, error
 }
 
 func (c *RedisBackend) Put(entry *Entry) error {
-	return fmt.Errorf("RedisBackend Put unimplemented")
+	encoded := base64.StdEncoding.EncodeToString(entry.Value)
+	realKey := fmt.Sprintf("%s/%s", c.path, entry.Key)
+	_, err := (*c.conn).Do("SET", realKey, encoded)
+	return err
 }
 
 func (c *RedisBackend) Get(key string) (*Entry, error) {
-	return nil, fmt.Errorf("RedisBackend Get unimplemented")
+	reply, err := (*c.conn).Do("GET", fmt.Sprintf("%s/%s", c.path, key))
+	if err != nil {
+		return nil, err
+	}
+	if reply == nil {
+		// No value at this key, so don't try to decode.
+		return nil, nil
+	}
+
+	replyStr, err := redis.String(reply, err)
+	if err != nil {
+		return nil, err
+	}
+
+	value, err := base64.StdEncoding.DecodeString(replyStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Entry{
+		Key: key,
+		Value: value,
+	}, nil
 }
 
 func (c *RedisBackend) Delete(key string) error {
-	return fmt.Errorf("RedisBackend Delete unimplemented")
+	_, err := (*c.conn).Do("DEL", fmt.Sprintf("%s/%s", c.path, key))
+	return err
 }
 
 func (c *RedisBackend) List(prefix string) ([]string, error) {
-	return nil, fmt.Errorf("RedisBackend List unimplemented")
+	// Construct a "directory"-style name with a trailing slash from
+	// `prefix`.  But`prefix` may be the empty string, so be careful
+	// how we add "/".
+	realPrefix := fmt.Sprintf("%s/%s", c.path, prefix)
+	if !strings.HasSuffix(realPrefix, "/") {
+		realPrefix += "/"
+	}
+	
+	// Ask Redis to list all keys beneath our prefix.  Note that this
+	// is not terribly efficient if you have a lot of keys, but we hope
+	// it's not a common operating.  There are more complex APIs for
+	// doing this incrementally.
+	reply, err := (*c.conn).Do("KEYS", realPrefix + "*")
+	matches, err := redis.Strings(reply, err)
+
+	// TODO - Don't recurse subdirectories.  This means stripping
+	// everything after "/" and removing duplicates.
+	results := make([]string, 0)
+	dirs := make(map[string]bool)
+	for _, match := range(matches) {
+		// Remove the path that we were querying from the results.
+		match = strings.TrimPrefix(match, realPrefix)
+
+		slashPos := strings.Index(match, "/")
+		if slashPos == -1 {
+			// We have an ordinary file, so return it directly.
+			results = append(results, match)
+		} else {
+			// Keep the trailing slash.
+			dir := match[0:slashPos+1]
+
+			// Have we seen this directory already?  If not,
+			// return it and mark it as seen.
+			if _, present := dirs[dir]; !present {
+				results = append(results, dir)
+				dirs[dir] = true
+			}
+		}
+	}
+
+	return results, err
 }
 
 func (c *RedisBackend) LockWith(key, value string) (Lock, error) {
